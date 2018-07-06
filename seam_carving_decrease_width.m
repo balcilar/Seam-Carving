b9f67d228ce8a261813dcb9aa8bbb59c6d86c596
Muhammet Balcilar <muhammetbalcilar@gmail.com>
2018-07-06 23:33:36 +0200

    Update seam_carving_decrease_width.m

--- seam_carving_decrease_width.m
+++ seam_carving_decrease_width.m
@@ -1,7 +1,7 @@
 clear all
 clc
 
-im=imread('inputSeamCarvingPrague.jpg');
+im=imread('input/Prague.jpg');
 
 energyImg = energy_img(im);
 
@@ -23,29 +23,3 @@ figure;imshow(im);
 title('Original Prague');
 figure;imshow(nim);
 title('100 width decreased Prague');
-
-
-
-im=imread('inputSeamCarvingMall.jpg');
-
-energyImg = energy_img(im);
-
-nim=im;
-nen=energyImg;
-for i=1:100
-    i
-    [nim,nen] = decrease_width(nim,nen);
-end
-
-imwrite(nim,'outputReduceWidthMall.png');
-
-figure;subplot(2,1,1);imshow(im);
-title('Original Mall');
-subplot(2,1,2);imshow(nim);
-title('100 width decreased Mall');
-
-figure;imshow(im);
-title('Original Mall');
-figure;imshow(nim);
-title('100 width decreased Mall');
-
