a8c5922565df3464507271bce6fe4f7c032b4366
Muhammet Balcilar <muhammetbalcilar@gmail.com>
2018-07-06 23:32:45 +0200

    Update seam_carving_decrease_height.m

--- seam_carving_decrease_height.m
+++ seam_carving_decrease_height.m
@@ -1,7 +1,7 @@
 clear all
 clc
 
-im=imread('inputSeamCarvingPrague.jpg');
+im=imread('input/Prague.jpg');
 
 energyImg = energy_img(im);
 
@@ -23,29 +23,3 @@ figure;imshow(im);
 title('Original Prague');
 figure;imshow(nim);
 title('50 height decreased Prague');
-
-
-
-im=imread('inputSeamCarvingMall.jpg');
-
-energyImg = energy_img(im);
-
-nim=im;
-nen=energyImg;
-for i=1:50
-    i
-    [nim,nen] = decrease_height(nim,nen);
-end
-
-imwrite(nim,'outputReduceHeightMall.png');
-
-figure;subplot(1,2,1);imshow(im);
-title('Original Mall');
-subplot(1,2,2);imshow(nim);
-title('50 height decreased Mall');
-
-figure;imshow(im);
-title('Original Mall');
-figure;imshow(nim);
-title('50 height decreased Mall');
-
